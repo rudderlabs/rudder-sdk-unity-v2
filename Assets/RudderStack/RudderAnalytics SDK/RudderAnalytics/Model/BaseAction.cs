1165bcddf61e291fb75c8aba2f2ce4ad1eb67332
Desu Sai Venkat <venkat@rudderstack.com>
2023-05-11 18:55:18 +0530

    fix: added missing originalTimestamp on the root of the payload

--- Assets/RudderStack/RudderAnalytics SDK/RudderAnalytics/Model/BaseAction.cs
+++ Assets/RudderStack/RudderAnalytics SDK/RudderAnalytics/Model/BaseAction.cs
@@ -18,6 +18,9 @@ namespace RudderStack.Model
         [JsonProperty(PropertyName = "timestamp")]
         public string Timestamp { get; private set; }
 
+        [JsonProperty(PropertyName = "originalTimestamp")]
+        public string originalTimestamp { get; private set;}
+
         [JsonProperty(PropertyName = "context")]
         public RudderContext Context { get; set; }
 
@@ -41,10 +44,11 @@ namespace RudderStack.Model
 
             this.Type = type;
             this.MessageId = Guid.NewGuid().ToString();
+            this.originalTimestamp = DateTime.UtcNow.ToString("o");
             if (options.Timestamp.HasValue)
                 this.Timestamp = options.Timestamp.Value.ToString("o");
             else
-                this.Timestamp = DateTime.UtcNow.ToString("o");
+                this.Timestamp = originalTimestamp;
             this.Context = options.Context;
             this.Integrations = options.Integrations;
             this.AnonymousId = options.AnonymousId;
