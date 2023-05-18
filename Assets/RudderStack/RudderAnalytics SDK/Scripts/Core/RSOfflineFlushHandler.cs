543e1e795b43a4d06a305f2d6b6fcb74d8ca0667
Artur Kashuba <40093366+Rockstrip@users.noreply.github.com>
2023-05-18 15:24:42 +0300

    config fix

---
+++ Assets/RudderStack/RudderAnalytics SDK/Scripts/Core/RSOfflineFlushHandler.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RudderStack.Flush;
+using RudderStack.Model;
+using RudderStack.Unity.Utility;
+using UnityEngine;
+
+namespace RudderStack.Unity
+{
+    public class RSOfflineFlushHandler : IAsyncFlushHandler
+    {
+        private readonly int              _dbThresholdCount;
+        private readonly RSStorageManager _storageManager;
+        private readonly List<BaseAction> _queue;
+
+        public RSOfflineFlushHandler(RSConfig config)
+        {
+            _queue            = new List<BaseAction>();
+            _dbThresholdCount = config.GetDbThresholdCount();
+            
+            const string keyLocation = "04j4bf5dkd8";
+            string       storageKey;
+            if (PlayerPrefs.HasKey(keyLocation))
+            {
+                storageKey = PlayerPrefs.GetString(keyLocation);
+            }
+            else
+            {
+                using var aes = System.Security.Cryptography.Aes.Create();
+                storageKey = Convert.ToBase64String(aes.Key);
+                PlayerPrefs.SetString(keyLocation, storageKey);
+            }
+            
+            _storageManager = new RSStorageManager(storageKey);
+
+            _queue.AddRange(_storageManager.LoadFromFile());
+        }
+
+        public void Process(BaseAction action)
+        {
+            _queue.Add(action);
+            if (_queue.Count > _dbThresholdCount) _queue.RemoveAt(0);
+            
+            _storageManager.SaveToFile(_queue);
+
+        }
+
+        public void Flush() { }
+        public Task FlushAsync() => Task.CompletedTask;
+        public void Dispose() { }
+    }
+}
